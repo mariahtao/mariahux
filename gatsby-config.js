16e86f22edd7b7b152a9b0724f8fb98e3b4a8157
mariahtao <mariahtao@gmail.com>
2022-04-06 17:19:20 -0700

    postcss

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,15 +3,5 @@ module.exports = {
     title: `mariahux`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [
-    {
-      resolve: `gatsby-plugin-sass`,
-      options: {
-        postCssPlugins: [
-          require("tailwindcss"),
-          require("./tailwind.config.js"), // Optional: Load custom Tailwind CSS configuration
-        ],
-      },
-    },
-  ],
+  plugins: [`gatsby-plugin-postcss`],
 }
