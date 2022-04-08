950309ed3e2f10bf40b2a27c218749116bb82642
mariahtao <mariahtao@gmail.com>
2022-04-07 17:45:28 -0700

    tailwind fixes

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
-import Sidebar from "./Sidebar";
-
+//import Sidebar from "./Sidebar";
 
 const Layout = ({ pageTitle, children }) => {
   return (
