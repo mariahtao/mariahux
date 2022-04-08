2ea4b7dcf806285a1fb16e18953031ef3cb329f4
mariahtao <mariahtao@gmail.com>
2022-04-07 19:49:11 -0700

    init sanity

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,21 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `mariahux`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [`gatsby-plugin-postcss`],
+  plugins: [
+    `gatsby-plugin-postcss`,
+    {
+      resolve: "gatsby-source-sanity",
+      options: {
+        projectId: process.env.SANITY_PROJECT_ID,
+        dataset: process.env.SANITY_DATASET,
+        token: process.env.SANITY_TOKEN,
+      },
+    }, 
+  ],
 }
