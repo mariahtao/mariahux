ac00f8e7f196d773fb478629ecbcb3998d1c9fdf
mariahtao <mariahtao@gmail.com>
2022-05-01 21:28:32 -0700

    casing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import * as React from "react";
-import Layout from "../components/layout";
-import Card from "../components/card";
+import Layout from "../components/Layout";
+import Card from "../components/Card";
 
 const IndexPage = () => {
   return (
